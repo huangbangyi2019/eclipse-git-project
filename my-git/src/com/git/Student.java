32c618844b557da560e793efea8584698fd6a0b4
huangbangyi2019 <1094476088@qq.com>
2020-05-07 15:31:05 +0800

    first create a git project

---
+++ my-git/src/com/git/Student.java
@@ -0,0 +1,48 @@
+package com.git;
+
+public class Student {
+
+	private int id;
+	private String username;
+	private String password;
+	private int age;
+	private int sex;
+	private String[] love;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public String[] getLove() {
+		return love;
+	}
+	public void setLove(String[] love) {
+		this.love = love;
+	}
+}
